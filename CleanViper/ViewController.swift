6e0bc34c603a9d804732d2666fbe5110b00bdc19
naelafauzul <naelafauzulm@gmail.com>
2024-04-11 14:41:18 +0700

    Initial Commit

---
+++ CleanViper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanViper
+//
+//  Created by Naela Fauzul Muna on 11/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
